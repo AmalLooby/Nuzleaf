4b60c15807f1d04019437a2effaca803a841d29f
Amal Looby <81536426+AmalLooby@users.noreply.github.com>
2022-01-29 19:31:58 +0000

    Added jumping

--- Nuzleaf/Assets/PlayerController.cs
+++ Nuzleaf/Assets/PlayerController.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Character Controller velocity, look sensitivity and smoothing
     [SerializeField] Transform playerCamera = null;
     [SerializeField] float mouseSensitivity = 3.5f;
     [SerializeField] float walkSpeed = 6.0f;
+    [SerializeField] float gravity = -13.0f;
     [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
     [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
 
+    // Character Controller Cursor Locking
     [SerializeField] bool lockCursor = true;
 
+    // Character Controller jumping
+    [SerializeField] AnimationCurve jumpFalloff;
+    [SerializeField] float jumpMultiplier;
+    [SerializeField] KeyCode jumpKey;
+
+    bool isJumping;
+
+    //Character Controller camera locking
     float cameraPitch = 0.0f;
+    float velocityY = 0.0f; 
     CharacterController controller = null;
 
+    // Character Controller 2D Vector States
     Vector2 currentDir = Vector2.zero;
     Vector2 currentDirVelocity = Vector2.zero;
 
@@ -37,6 +49,7 @@ public class PlayerController : MonoBehaviour
     {
         UpdateMouseLook();
         UpdateMovement();
+        UpdateGravity();
     }
 
     void UpdateMouseLook()
@@ -59,9 +72,48 @@ public class PlayerController : MonoBehaviour
 
         currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
 
-        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed;
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed + Vector3.up * velocityY;
 
         controller.Move(velocity * Time.deltaTime);
 
+       UpdateJump();
+    }
+
+    void UpdateJump()
+    {
+        if(Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        controller = GetComponent<CharacterController>();
+        controller.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+
+        do
+        {
+            float jumpForce = jumpFalloff.Evaluate(timeInAir);
+            controller.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+
+        } while (!controller.isGrounded && controller.collisionFlags != CollisionFlags.Above);
+
+        controller.slopeLimit = 45.0f;
+        isJumping = false;
+
+    }
+
+    void UpdateGravity()
+    {
+        if (controller.isGrounded)
+            velocityY = 0.0f;
+
+        velocityY += gravity * Time.deltaTime;
     }
 }
